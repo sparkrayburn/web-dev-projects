d14df4d91e0cd051717fc11062bbfc7d94b646b8
sparkrayburn <abhinavdotkulkarni@gmail.com>
2023-03-28 17:22:24 +0530

    hashing encryption

--- secrets challenge/app.js
+++ secrets challenge/app.js
@@ -3,7 +3,7 @@ const express = require("express");
 const bodyParser = require("body-parser");
 const ejs = require("ejs");
 const mongoose = require('mongoose');
-const encrypt = require('mongoose-encryption');
+const md5 = require("md5");
 mongoose.set('strictQuery', true);
  //encryption using mongoose
 mongoose.connect('mongodb://127.0.0.1:27017/userDB', { useNewUrlParser: true, useUnifiedTopology: true })
@@ -32,7 +32,7 @@ const userSchema = new mongoose.Schema({
 
 
 
-userSchema.plugin(encrypt, {secret: process.env.SECRET, encryptedFields: ["password"]});
+
 
 const User = new mongoose.model("User", userSchema);
 
@@ -59,7 +59,7 @@ app.get("/", function (req, res) {
 app.post("/register", function (req, res) { 
   const newUser = new User({
     email : req.body.username,
-    password: req.body.password
+    password: md5(req.body.password)
   });
 
   newUser.save()
@@ -73,7 +73,7 @@ app.post("/register", function (req, res) {
 
 app.post("/login", function (req, res) {
   const username = req.body.username;
-  const password = req.body.password;
+  const password = md5(req.body.password);
   
   User.findOne({email: username})
   .then(function (founduser) {
